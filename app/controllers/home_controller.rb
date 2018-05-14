167c87b465b637c09420b3866ffe9f766ca374ba
Fadi Yousef <fadi.yousef@live.com>
2018-05-14 22:39:29 +0300

    lookup form code and style

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,4 +11,23 @@ class HomeController < ApplicationController
 
   def about
   end
+
+  def lookup
+
+  	require 'net/http'
+  	require 'json'
+  	@url = 'https://api.coinmarketcap.com/v2/ticker/'
+  	@uri = URI(@url)
+  	@response = Net::HTTP.get(@uri)
+  	@lookup_coins = JSON.parse(@response)
+
+  	@symbol = params[:sym]
+  	if @symbol
+  		@symbol = @symbol.upcase
+  	end
+  	if @symbol == ""
+  		@symbol = "Please enter a currency symbol!"
+  	end
+  end
+
 end
